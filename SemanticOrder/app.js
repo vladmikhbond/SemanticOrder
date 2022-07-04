5c1ee49ace2aa208df1044c50b71457f3edd14bd
volodymyr bondariev <vladmikhbond@hotmail.com>
2022-07-04 12:12:57 +0300

    .

--- SemanticOrder/app.js
+++ SemanticOrder/app.js
@@ -5,15 +5,18 @@ const utils_js_1 = require("./src/utils.js");
 const parts = new Parts_js_1.Parts();
 parts.bodyFromAllLects();
 parts.findDeps();
-console.log(parts);
-console.log("---------------------------");
-for (let p of parts._parts) {
-    console.log(p.id);
-    for (let d of p.deps) {
-        let letColor = d.len < 0 ? utils_js_1.color.red : utils_js_1.color.green;
-        console.log('        ' + utils_js_1.color.white + d.partId + ' ' +
-            utils_js_1.color.yellow + d.marker + ' ' +
-            letColor + d.len + utils_js_1.color.white);
+show(parts);
+function show(parts) {
+    for (let p of parts._parts) {
+        console.log('@2', p.id);
+        if (p.deps.length == 0)
+            console.log('        -');
+        for (let d of p.deps) {
+            let letColor = d.len < 0 ? utils_js_1.color.red : utils_js_1.color.green;
+            console.log('        ' + utils_js_1.color.white + d.partId + ' ' +
+                utils_js_1.color.yellow + d.marker + ' ' +
+                letColor + d.len + utils_js_1.color.white);
+        }
     }
 }
 //# sourceMappingURL=app.js.map
--- SemanticOrder/app.ts
+++ SemanticOrder/app.ts
@@ -1,20 +1,22 @@
-﻿
-import { Part, Parts } from './src/Parts.js';
+﻿import { Parts } from './src/Parts.js';
 import { color } from "./src/utils.js";
 
 const parts = new Parts();
 parts.bodyFromAllLects();
 parts.findDeps();
+show(parts);
 
-console.log(parts);
-console.log("---------------------------");
 
-for (let p of parts._parts) {
-   console.log(p.id);
-   for (let d of p.deps) {
-      let letColor: string = d.len < 0 ? color.red : color.green;
-      console.log('        ' + color.white + d.partId + ' ' +
-         color.yellow + d.marker + ' ' +
-         letColor + d.len + color.white);
+
+function show(parts) {
+   for (let p of parts._parts) {
+      console.log('@2', p.id);
+      if (p.deps.length == 0) console.log('        -');
+      for (let d of p.deps) {
+         let letColor: string = d.len < 0 ? color.red : color.green;
+         console.log('        ' + color.white + d.partId + ' ' +
+            color.yellow + d.marker + ' ' +
+            letColor + d.len + color.white);
+      }
    }
 }
