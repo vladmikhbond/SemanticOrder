1b84d271f663b8d3457400a5047a5cc8cc6aa1d7
volodymyr bondariev <vladmikhbond@hotmail.com>
2022-08-11 17:58:50 +0300

    '

--- SemanticOrder/app.js
+++ SemanticOrder/app.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Parts_js_1 = require("./src/Parts.js");
 const View_js_1 = require("./src/View.js");
-const parts = new Parts_js_1.Parts('../data/js/');
+const parts = new Parts_js_1.Parts('../data/oop/');
 (0, View_js_1.toFiles)(parts, 'excel_concepts.txt', 'excel_parts.txt');
 //# sourceMappingURL=app.js.map
--- SemanticOrder/app.ts
+++ SemanticOrder/app.ts
@@ -2,6 +2,6 @@
 import { toFiles } from "./src/View.js";
 
 
-const parts = new Parts('../data/js/');
+const parts = new Parts('../data/oop/');
 
 toFiles(parts, 'excel_concepts.txt', 'excel_parts.txt');
